541cabada2b487a476a411c55856d0928d5f8493
Joakim Torsvik <jotto-96@hotmail.com>
2020-10-03 23:04:04 +0200

    Navbar med hjemmeskjerm og en "om-oss" skjerm

--- App.js
+++ App.js
@@ -1,5 +1,40 @@
-import React from 'react';
-import "react-native-gesture-handler";
-import {NavigationContainer} from "@react-navigation/native"
-import Navigator from "./routes/home_stack";
+import React from 'react'
+import { View, Text, Button} from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+const Tab = createBottomTabNavigator();
+
+
+function HomeScreen(){
+    return(
+        <View style = {{flex:1, alignItems:"center", justifyContent:"center"}}>
+            <Text>Home Screen</Text>
+        </View>
+    );
+}
+
+function AboutScreen(){
+    return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>About screen</Text>
+    </View>
+    );
+}
+
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator initialRoutenName = "Home">
+                <Tab.Screen name = "Home" component = {HomeScreen}/>
+                <Tab.Screen name = "About" component = {AboutScreen}/>
+            </Tab.Navigator>
+        </NavigationContainer> 
+    );
+}
+
+
+
 
--- screens/about.js
+++
@@ -1,3 +0,0 @@
-return (
-    <text>about</text>
-)
