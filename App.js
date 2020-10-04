902a721a883713433dd1ba558fb5de4ca543518e
Joakim Torsvik <jotto-96@hotmail.com>
2020-10-04 15:47:32 +0200

    Lagt til navigasjonsbar med tre ulike "screens".
    De ulike vinduene som er lagt til heter "Map, "Overview" og "About".
    
    Dette er bare forslag til navn.
    
    Navigering mellom de fungerer, men skal sjekke litt mer hvordan det funker.
    
    Begynt på arbeidet med å få inn en liste av de ulike vannene på ene vinduet.
    
    La til Firebase dependencies, men har ikke begynt noe mer der.

--- App.js
+++ App.js
@@ -1,24 +1,38 @@
 import React from 'react'
-import { View, Text, Button} from "react-native"
+import { View } from "react-native"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons'; 
+import MapScreen from "./screens/mapScreen";
+import AboutScreen from "./screens/aboutScreen"
+import OverviewScreen from "./screens/overviewScreen"
 
 
-const Tab = createBottomTabNavigator();
+
+const Tab = createBottomTabNavigator()
 
 
-function HomeScreen(){
+function DisplayMapScreen(){
     return(
         <View style = {{flex:1, alignItems:"center", justifyContent:"center"}}>
-            <Text>Home Screen</Text>
+            <MapScreen/>
         </View>
     );
 }
 
-function AboutScreen(){
+function DisplayOverviewScreen(){
     return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>About screen</Text>
+    <View>
+        <OverviewScreen/>
+    </View>
+    );
+}
+
+
+function DisplayAboutScreen(){
+    return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <AboutScreen/>
     </View>
     );
 }
@@ -27,9 +41,37 @@ function AboutScreen(){
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator initialRoutenName = "Home">
-                <Tab.Screen name = "Home" component = {HomeScreen}/>
-                <Tab.Screen name = "About" component = {AboutScreen}/>
+            <Tab.Navigator 
+            initialRoutenName = "Map"
+            tabBarOptions ={{
+                activeTintColor: "green"
+            }}
+            >
+                <Tab.Screen 
+                name = "Map" 
+                component = {DisplayMapScreen}
+                options ={{
+                    tabBarIcon: () => (
+                        <AntDesign name="home" size={24} color="black" />
+                    )
+                }}
+                />
+                <Tab.Screen 
+                name = "Overview" 
+                component = {DisplayOverviewScreen}
+                options={{
+                    tabBarIcon: () => (
+                        <AntDesign name="plus" size={24} color="black" />
+                    )
+                }}/>
+                <Tab.Screen 
+                name = "About" 
+                component = {DisplayAboutScreen}
+                options={{
+                    tabBarIcon: () => (
+                        <AntDesign name="info" size={24} color="black"/>
+                    )
+                }}/>
             </Tab.Navigator>
         </NavigationContainer> 
     );
