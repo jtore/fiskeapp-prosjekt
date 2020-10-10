891a3869b29e8c5b9eedd5ba263f6bd7fa126a6e
MadsEyv <madsskole97@gmail.com>
2020-10-10 15:15:46 +0200

    Added functional map to mapscreen.js

--- screens/mapScreen.js
+++ screens/mapScreen.js
@@ -1,11 +1,26 @@
-import React from 'react'
-import {View, Text} from "react-native"
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, View, Dimensions, Text,TouchableOpacity } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+ container: {
+ flex: 1,
+ justifyContent: 'flex-end',
+ alignItems: 'center',
+ },
+ map: {
+ ...StyleSheet.absoluteFillObject,
+ },
+});
 
 export default function MapScreen() {
-    return (
-        <View>
-        <Text>Map</Text>
-        </View>
-    )
-
-}
+    return <MapView
+ style={styles.map}
+ initialRegion={{
+ latitude: 25.6989,
+ longitude: 32.6421,
+ latitudeDelta: 0.0491,
+ longitudeDelta: 0.0375,
+}}>
+</MapView>
+};
