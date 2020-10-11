2bc6832afb6f4ebabae620007f2f830b072a3d96
Joakim Torsvik <jotto-96@hotmail.com>
2020-10-12 00:39:15 +0200

    La til marker

--- screens/mapScreen.js
+++ screens/mapScreen.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, View, Dimensions, Text,TouchableOpacity } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-
-
-
+import { Marker } from "react-native-maps";
 
 
 export default function MapScreen() {
@@ -16,8 +14,12 @@ export default function MapScreen() {
         latitudeDelta: 0.0491,
         longitudeDelta: 0.0375,
         }}
-        
         >
+        <MapView.Marker
+            coordinate={{latitude:60.397076, longitude: 5.324383}}
+            title = {"Bergen!"}
+            description = {"Test test"}
+        />
     </MapView>
 };
 
